96d8758ac1b5e654d35b06bb56a5f6ae507da34e
markoMagdy1 <markomagdy1231@gmail.com>
2024-09-18 14:15:12 +0300

    test

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,15 +3,41 @@ import styles from "./footer.module.css";
 
 export const Footer = () => {
   return (
-    <div className="row">
+    <footer className={`${styles.footer} row jutify-content-center m-5`}>
+      {/* =============== first column =============== */}
       <div className="col-6 col-md-3">
-        <a href="">
-          <img src="./assets/images/footer-logo.png" alt="" />
-        </a>
+        <div className="icon_parent">
+          <a href="" className={`${styles.footer_iocn}`}>
+            <img src="./assets/images/footer-logo.png" alt="footer-icon" />
+          </a>
+          <div className={`${styles.footer_content}`}>
+            A fast-growing diversified holding company that is redefining the
+            global marketplace.
+          </div>
+        </div>
       </div>
-      <div className="col-6 col-md-3"></div>
-      <div className="col-6 col-md-3"></div>
-      <div className="col-6 col-md-3"></div>
-    </div>
+      {/* =============== 2nd column =============== */}
+      <div className="col-6 col-md-3">
+        <div className={`${styles.footer_title}`}>banknote Identity</div>
+        <div className={`${styles.footer_content}`}>Who We Are</div>
+        <div className={`${styles.footer_content}`}>what we do</div>
+        <div className={`${styles.footer_content}`}>leadership</div>
+      </div>
+      {/* =============== 3rd column =============== */}
+      <div className="col-6 col-md-3">
+        <div className={`${styles.footer_title}`}>find out more</div>
+        <div className={`${styles.footer_content}`}>Businesses</div>
+        <div className={`${styles.footer_content}`}>INVESTOR RELATIONS</div>
+        <div className={`${styles.footer_content}`}>leadership</div>
+      </div>
+      {/* =============== 4rd column =============== */}
+      <div className="col-6 col-md-3">
+        <div className={`${styles.footer_title}`}>security</div>
+        <div className={`${styles.footer_content}`}>privacy policy</div>
+        <div className={`${styles.footer_content}`}>terms & conditions</div>
+        <div className={`${styles.footer_content}`}>cookie policy</div>
+      </div>
+
+    </footer>
   );
 };
